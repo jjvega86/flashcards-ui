26c18ef92cbc35259a934d567019fbd81d85a9b3
JJ <jj.vega86@gmail.com>
2021-02-14 19:58:33 -0500

    Finished sidebar styling and added cardDisplay component to project

---
+++ src/components/CardDisplay/cardDisplay.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './cardDisplay.css';
+
+class CardDisplay extends Component{
+    constructor(props){
+        super(props);
+        this.state = {}
+    }
+
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default CardDisplay;
--- src/components/NavBar/navbar.jsx
+++ src/components/NavBar/navbar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import './navbar.css';
 
 const NavBar = () => {
   return (
     <div>
-      <nav class="navbar navbar-dark bg-success">
-        <span class="navbar-brand mb-0 h1">TechCollect</span>
+      <nav className="navbar navbar-dark bg-success">
+        <span className="navbar-brand mb-0 h1">TechCollect</span>
       </nav>
     </div>
   );
