460f6ae5b427355ff1854733cc2ef5e7c280f123
JJ <jj.vega86@gmail.com>
2021-02-16 14:58:45 -0500

    Added styling for Card component, initial JSX, and stateful variables to CardDisplay along with initial rendered JSX for displaying a Card with buttons

--- src/components/CardDisplay/Card/card.jsx
+++ src/components/CardDisplay/Card/card.jsx
@@ -1,16 +1,13 @@
-import React, {Component} from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
 
-class Card extends Component{
-    constructor(props){
-        super(props);
-    }
-
-    render(){
-        return(
-            <div></div>
-        )
-    }
-}
+const Card = (props) => {
+  return (
+  <div className="cardStyle">
+    <h3>Title</h3>
+    <h5>Description</h5>
+  </div>
+  );
+};
 
 export default Card;
--- src/components/CardDisplay/cardDisplay.jsx
+++ src/components/CardDisplay/cardDisplay.jsx
@@ -1,16 +1,31 @@
 import React, { Component } from "react";
 import "./CardDisplay.css";
+import Card from './Card/Card';
 
 class CardDisplay extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      activeCollection: props.activeCollection,
+      currentCard: ''
+    };
   }
 
   render() {
     return (
-      <div>
-        <h1>The Card Will Be Displayed Here</h1>
+      <div className="container">
+      <div className="row">
+        <div className="col-md-4">
+          <button></button>
+        </div>
+        <div className="col-md-4">
+          <Card />
+        </div>
+        <div className="col-md-4">
+          <button></button>
+        </div>
+      </div>
+        
       </div>
     );
   }
