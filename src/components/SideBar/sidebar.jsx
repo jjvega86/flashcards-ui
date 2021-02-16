0b61c79a6e0140edb632158193d5b921389ed9d6
JJ <jj.vega86@gmail.com>
2021-02-16 08:08:21 -0500

    Added activeStyle object and conditionals to sidebar for active collection styling

--- src/components/SideBar/sidebar.jsx
+++ src/components/SideBar/sidebar.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import "./sidebar.css";
 
+const activeStyle = {
+    backgroundColor: 'green' 
+}
+
 const SideBar = (props) => {
+    let activeIndex = props.activeCollection;
     const links = props.collections.map((item, index) => {
-        return <a key={index} onClick={() => props.makeActive(index)} href="#collection3">{item.title}</a>
+        if(index === activeIndex){
+            return <a key={index} style={activeStyle} onClick={() => props.makeActive(index)} href="#collection3">{item.title}</a>
+        } else {
+            return <a key={index} onClick={() => props.makeActive(index)} href="#collection3">{item.title}</a>
+        }        
     })
     console.log(links);
   return (
