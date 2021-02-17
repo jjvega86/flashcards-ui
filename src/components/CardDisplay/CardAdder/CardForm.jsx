8672e17aafc7b50338784730aa38b579e861378e
JJ <jj.vega86@gmail.com>
2021-02-17 15:59:15 -0500

    Added CardForm class component and stylesheet to project

---
+++ src/components/CardDisplay/CardAdder/CardForm.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './CardForm.css';
+
+class CardForm extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default CardForm;
