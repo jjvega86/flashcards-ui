829168c1a24abcc2efb703f4485c6ae667d87f09
JJ <jj.vega86@gmail.com>
2021-02-17 16:19:20 -0500

    Added initial logic to CardForm, including change and submit handlers

--- src/components/CardDisplay/CardAdder/CardForm.jsx
+++ src/components/CardDisplay/CardAdder/CardForm.jsx
@@ -1,18 +1,49 @@
-import React from 'react';
-import './CardForm.css';
+import React from "react";
+import "./CardForm.css";
+import axios from "axios";
 
-class CardForm extends React.Component{
-    constructor(props){
-        super(props);
-    }
+// this class will accept the id of the current collection and post both title and description to the collection
+// using a POST call in Axios on the handleSubmit function
 
-    render(){
-        return(
-            <div>
-                
-            </div>
-        )
-    }
+class CardForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+        title: "",
+        description: "" 
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({ value: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+      const card = {
+          title: this.state.title,
+          description: this.state.description
+      }
+  };
+
+  addNewCard = (card) => {
+
+  }
+
+  render() {
+    return (
+      <form>
+        <label>
+          Card Title:
+          <textarea type="title" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <label>
+          Card Description:
+          <textarea type="description" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
 export default CardForm;
